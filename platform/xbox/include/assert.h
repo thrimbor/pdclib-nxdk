18e3b16e3bf5d73dbfe842c95c5bb6b0ee100a01
Stefan Schmidt <thrimbor.github@gmail.com>
2018-11-18 08:02:23 +0100

    xbox: Replace assert() with system-specific version

---
+++ platform/xbox/include/assert.h
@@ -0,0 +1,39 @@
+/* Diagnostics <assert.h>
+
+   This file is part of the Public Domain C Library (PDCLib).
+   Permission is granted to use, modify, and / or redistribute at will.
+*/
+
+#include "pdclib/_PDCLIB_aux.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _PDCLIB_ASSERT_H
+#define _PDCLIB_ASSERT_H
+void _xbox_assert(char const * const expression, char const * const file_name, char const * const function_name, unsigned long line);
+#endif
+
+/* If NDEBUG is set, assert() is a null operation. */
+#undef assert
+
+#ifdef NDEBUG
+    #define assert(ignore) ((void)0)
+#else
+    #define assert(expression) \
+        do { \
+            if(!(expression)) { \
+                _xbox_assert(_PDCLIB_symbol2string(expression), \
+                             __FILE__, \
+                             __func__, \
+                             __LINE__); \
+            } \
+        } while(0)
+#endif
+
+#define static_assert _Static_assert
+
+#ifdef __cplusplus
+}
+#endif
