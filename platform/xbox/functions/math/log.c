b3207ef1c80b3c2cbae14e57a3f18142df7c4074
Stefan Schmidt <thrimbor.github@gmail.com>
2020-06-21 00:53:21 +0200

    xbox: Fix incorrect behavior of log, exp and pow functions

--- platform/xbox/functions/math/log.c
+++ platform/xbox/functions/math/log.c
@@ -2,30 +2,24 @@
 
 double log(double x)
 {
-    if (x <= 0.0) return NAN;
-
-    __asm__ ("fyl2x;"
-             "fldl2e;"
-             "fdivrp" : "=t"(x) : "0"(x), "u"(1.0));
+    __asm__ ("fldln2;"
+             "fxch;"
+             "fyl2x;" : "=t"(x) : "0"(x));
     return x;
 }
 
 float logf(float x)
 {
-    if (x <= 0.0f) return NAN;
-
-    __asm__ ("fyl2x;"
-             "fldl2e;"
-             "fdivrp" : "=t"(x) : "0"(x), "u"(1.0f));
+    __asm__ ("fldln2;"
+             "fxch;"
+             "fyl2x;" : "=t"(x) : "0"(x));
     return x;
 }
 
 long double logl(long double x)
 {
-    if (x <= 0.0) return NAN;
-
-    __asm__ ("fyl2x;"
-             "fldl2e;"
-             "fdivrp" : "=t"(x) : "0"(x), "u"(1.0));
+    __asm__ ("fldln2;"
+             "fxch;"
+             "fyl2x;" : "=t"(x) : "0"(x));
     return x;
 }
--- platform/xbox/functions/math/pow.c
+++ platform/xbox/functions/math/pow.c
@@ -2,15 +2,48 @@
 
 double pow(double x, double y)
 {
-    return exp(y*log(x));
+    if (x < 0.0 && y != trunc(y)) {
+        // Negative base and non-integer exponent
+        return -NAN;
+    }
+
+    float r = exp(y * log(fabs(x)));
+
+    if (x < 0.0 && remainder(y, 2.0) != 0) {
+        r = -r;
+    }
+
+    return r;
 }
 
 float powf(float x, float y)
 {
-    return expf(y*logf(x));
+    if (x < 0.0f && y != truncf(y)) {
+        // Negative base and non-integer exponent
+        return -NAN;
+    }
+
+    float r = expf(y * logf(fabsf(x)));
+
+    if (x < 0.0f && remainderf(y, 2.0f) != 0) {
+        r = -r;
+    }
+
+    return r;
 }
 
 long double powl(long double x, long double y)
 {
-    return expl(y*logl(x));
+    if (x < 0.0 && y != truncl(y)) {
+        // Negative base and non-integer exponent
+        return -NAN;
+    }
+
+    float r = expl(y * logl(fabsl(x)));
+
+    if (x < 0.0 && remainderl(y, 2.0) != 0) {
+        r = -r;
+    }
+
+    return r;
 }
