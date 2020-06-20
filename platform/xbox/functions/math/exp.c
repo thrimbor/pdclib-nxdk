cd0d6ff25371dbd8eed403fa7872fe66f1a39365
Stefan Schmidt <thrimbor.github@gmail.com>
2020-06-21 00:53:21 +0200

    xbox: Fix incorrect behavior of log, exp and pow functions

--- platform/xbox/functions/math/exp.c
+++ platform/xbox/functions/math/exp.c
@@ -1,7 +1,18 @@
 #include <math.h>
+#include <stdint.h>
 
 double exp(double x)
 {
+    uint32_t *raw_bits = (uint32_t *)&x;
+    if ((raw_bits[1] & 0x7FFFFFFF) >= 0x7FF00000) {
+        // value is -INFINITY, INFINITY, -NAN or NAN
+        if (raw_bits[1] == 0xFFF00000 && raw_bits[0] == 0) {
+            // value is -INFINITY
+            return 0;
+        }
+        return x;
+    }
+
     __asm__ ("fldl2e;"
              "fmulp;"
              "fld %%st(0);"
@@ -19,6 +30,16 @@ double exp(double x)
 
 float expf(float x)
 {
+    uint32_t *raw_bits = (uint32_t *)&x;
+    if ((*raw_bits & 0x7FFFFFFF) >= 0x7F800000) {
+        // value is -INFINITY, INFINITY, -NAN or NAN
+        if (*raw_bits == 0xFF800000) {
+            // value is -INFINITY
+            return 0;
+        }
+        return x;
+    }
+
     __asm__ ("fldl2e;"
              "fmulp;"
              "fld %%st(0);"
@@ -36,6 +57,16 @@ float expf(float x)
 
 long double expl(long double x)
 {
+    uint32_t *raw_bits = (uint32_t *)&x;
+    if ((raw_bits[1] & 0x7FFFFFFF) >= 0x7FF00000) {
+        // value is -INFINITY, INFINITY, -NAN or NAN
+        if (raw_bits[1] == 0xFFF00000 && raw_bits[0] == 0) {
+            // value is -INFINITY
+            return 0;
+        }
+        return x;
+    }
+
     __asm__ ("fldl2e;"
              "fmulp;"
              "fld %%st(0);"
