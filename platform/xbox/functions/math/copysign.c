ccba5762eb31d0577620f6b94a368ecfb47a7aa9
Stefan Schmidt <thrimbor.github@gmail.com>
2020-06-21 11:56:56 +0200

    xbox: Implement copysign

--- platform/xbox/functions/math/copysign.c
+++ platform/xbox/functions/math/copysign.c
@@ -1,20 +1,30 @@
 #include <math.h>
 #include <assert.h>
+#include <stdint.h>
 
 double copysign(double x, double y)
 {
-    assert(0); // Not implemented
-    return 0.0;
+    uint32_t *x_raw = (uint32_t *)&x;
+    uint32_t *y_raw = (uint32_t *)&y;
+
+    x_raw[1] = (x_raw[1] & 0x7FFFFFFF) | (y_raw[1] & 0x80000000);
+    return x;
 }
 
 float copysignf(float x, float y)
 {
-    assert(0); // Not implemented
-    return 0.0;
+    uint32_t *x_raw = (uint32_t *)&x;
+    uint32_t *y_raw = (uint32_t *)&y;
+
+    *x_raw = (*x_raw & 0x7FFFFFFF) | (*y_raw & 0x80000000);
+    return x;
 }
 
 long double copysignl(long double x, long double y)
 {
-    assert(0); // Not implemented
-    return 0.0;
+    uint32_t *x_raw = (uint32_t *)&x;
+    uint32_t *y_raw = (uint32_t *)&y;
+
+    x_raw[1] = (x_raw[1] & 0x7FFFFFFF) | (y_raw[1] & 0x80000000);
+    return x;
 }
