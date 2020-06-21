440b854dcf3ec36fca4bcc9c8559e837940c1e7c
Stefan Schmidt <thrimbor.github@gmail.com>
2020-06-21 11:57:11 +0200

    xbox: Implement remainder

--- platform/xbox/functions/math/remainder.c
+++ platform/xbox/functions/math/remainder.c
@@ -3,18 +3,18 @@
 
 double remainder(double x, double y)
 {
-    assert(0); // Not implemented
-    return 0.0;
+    __asm__ ("fprem1;" : "=t"(x) : "0"(x), "u"(y));
+    return x;
 }
 
 float remainderf(float x, float y)
 {
-    assert(0); // Not implemented
-    return 0.0;
+    __asm__ ("fprem1;" : "=t"(x) : "0"(x), "u"(y));
+    return x;
 }
 
 long double remainderl(long double x, long double y)
 {
-    assert(0); // Not implemented
-    return 0.0;
+    __asm__ ("fprem1;" : "=t"(x) : "0"(x), "u"(y));
+    return x;
 }
