bbe0ad49073a1d43ef2b2bc84cd2608f2bce3273
Stefan Schmidt <thrimbor.github@gmail.com>
2021-03-25 16:44:29 +0100

    xbox: Use IMAGE_TLS_DIRECTORY_32 from winapi

--- platform/xbox/tls.c
+++ platform/xbox/tls.c
@@ -1,5 +1,5 @@
-#include <xboxkrnl/xboxkrnl.h>
-#include <pdclib/_PDCLIB_xbox_tls.h>
+#include <stdlib.h>
+#include <winnt.h>
 
 #pragma comment(linker, "/include:__tls_index")
 ULONG _tls_index = 0;
@@ -7,7 +7,7 @@ ULONG _tls_index = 0;
 __attribute__((section(".tls"))) char _tls_start = 0;
 __attribute__((section(".tls$ZZZ"))) char _tls_end = 0;
 
-const IMAGE_TLS_DIRECTORY _tls_used =
+const IMAGE_TLS_DIRECTORY_32 _tls_used =
 {
     (DWORD) &_tls_start,
     (DWORD) &_tls_end,
