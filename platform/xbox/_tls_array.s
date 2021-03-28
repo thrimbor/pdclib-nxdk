9ea64832ac0882b7d5519d2f1a9827a40fa822ed
Stefan Schmidt <thrimbor.github@gmail.com>
2021-03-28 22:59:54 +0200

    xbox: Make _tls_array.s SafeSEH-compatible

--- platform/xbox/_tls_array.s
+++ platform/xbox/_tls_array.s
@@ -1,3 +1,13 @@
+// This creates a special symbol with the least significant bit set.
+// It signals to the linker that this code is SafeSEH-compatible.
+.text
+.def @feat.00;
+.scl  3; // IMAGE_SYM_CLASS_STATIC
+.type 0; // IMAGE_SYM_TYPE_NULL
+.endef
+.globl @feat.00
+.set @feat.00, 1
+
 .data
 // _tls_array is a constant describing the offset of StackBase inside the NT TEB
 .globl __tls_array
