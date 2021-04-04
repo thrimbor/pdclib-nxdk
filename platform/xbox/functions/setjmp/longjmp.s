93a5c84ca034781ced729750183094c39b16a3d2
Stefan Schmidt <thrimbor.github@gmail.com>
2021-04-04 23:06:36 +0200

    xbox: Make setjmp/longjmp SafeSEH-compatible

--- platform/xbox/functions/setjmp/longjmp.s
+++ platform/xbox/functions/setjmp/longjmp.s
@@ -1,3 +1,14 @@
+// This creates a special symbol with the least significant bit set.
+// It signals to the linker that this code is SafeSEH-compatible.
+.text
+.def @feat.00;
+.scl  3; // IMAGE_SYM_CLASS_STATIC
+.type 0; // IMAGE_SYM_TYPE_NULL
+.endef
+.globl @feat.00
+.set @feat.00, 1
+
+
 .text
 
 /*
